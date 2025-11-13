91762a5b0101671b29b736c5e1bf6808fe38e1c5
pppoly <pppoly@gmail.com>
2025-11-13 10:20:29 +0900

    Configure base path in vite.config.js
    Set the base path for the project.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/socialmore-website/',  // 这里非常关键！！
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0'
-  }
 });
